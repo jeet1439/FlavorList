c7e1399236fa26a03b2f7b3518f78fedea1d1109
jeet <jeetbanik332004@gmail.com>
2025-05-12 00:35:33 +0530

    order-page-v1

--- frontend/src/pages/orders.jsx
+++ frontend/src/pages/orders.jsx
@@ -4,6 +4,7 @@ import { toast } from "react-hot-toast";
 import { Link } from 'react-router-dom';
 import { ShoppingCart } from "lucide-react";
 import axios from "axios";
+import { X } from 'lucide-react';
 
 export default function orders() {
    const [orders, setOrders] = useState([]);
@@ -30,7 +31,7 @@ export default function orders() {
 
    if(currentUser == null) {
     return (
-        <div className="flex items-center justify-center h-screen bg-stone-900">
+        <div className="flex items-center justify-center h-screen ">
         <Link
           to="/menu"
           className="text-yellow-500 hover:underline text-xl flex items-center gap-2"
@@ -42,14 +43,42 @@ export default function orders() {
     );
   }
   return (
-    <div>
-      {Array.isArray(orders) && orders.length > 0 ? (
-          orders.map(order => (
-            <div key={order.order_id}>{order.product_name}</div>
-          ))
-        ) : (
-          <p>No orders found.</p>
-        )}
-    </div>
-  )
+    <div className=' min-h-screen w-full bg-slate-950 '>
+  <div className="max-w-4xl mx-auto p-4 mt-10 min-h-screen bg-slate-950">
+    <h2 className="text-2xl font-semibold mb-4">Hey {currentUser.name}, you ordered </h2>
+    {Array.isArray(orders) && orders.length > 0 ? (
+      <div className="space-y-4">
+        {orders.map(order => (
+          <div
+            key={order.order_id}
+            className="flex items-center gap-4 bg-gradient-to-r from-black via-black/80 to-transparent shadow-md rounded-xl p-4 hover:shadow-lg transition"
+          >
+            <img
+              src={order.product_image}
+              alt={order.product_name}
+              className="w-20 h-20 object-cover rounded-md"
+            />
+            <div className="flex-1 mx-9">
+              <h3 className="text-lg font-medium">{order.product_name}</h3>
+              <p className="text-sm text-gray-200">{order.quantity} @ ₹{order.product_price}</p>
+            </div>
+            <div className="mr-3">
+              <p className="text-md text-gray-200 font-semibold">Total: ₹{order.total_price}</p>
+            </div>
+            <div className='text-right'>
+              <button className='flex items-center justify-center hover:text-red-700'>
+                <X className="w-5 h-5" />
+              </button>
+            </div>
+          </div>
+        ))}
+      <hr className='text-gray-500'/>
+      </div>
+    ) : (
+      <p className="text-gray-500 text-center">No orders found.</p>
+    )}
+  </div>
+  </div>
+);
+
 }
